c2f92a7e35b2014074759c222db94af745ad10f3
Remco Haszing <remcohaszing@gmail.com>
2017-02-18 20:27:55 +0100

    Add some minimal tests

---
+++ test_wakeonlan.py
@@ -0,0 +1,180 @@
+"""
+Tests for wakeonlan.
+
+"""
+import socket
+
+import pytest
+from mock import call
+from mock import patch
+
+from wakeonlan import create_magic_packet
+from wakeonlan import send_magic_packet
+
+
+@pytest.mark.parametrize('mac,packet', [
+    ('000000000000',
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'
+     b'\x00\x00\x00\x00\x00\x00'),
+    ('01:23:45:67:89:ab',
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'
+     b'\x01#Eg\x89\xab'),
+    ('ff-ff-ff-ff-ff-ff',
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'
+     b'\xff\xff\xff\xff\xff\xff'),
+], ids=['no separator', 'colons', 'hyphens'])
+def test_create_magic_packet(mac, packet):
+    """
+    Test whether a correct magic packet is created.
+
+    """
+    result = create_magic_packet(mac)
+    assert result == packet
+
+
+@patch('socket.socket', autospec=True)
+def test_send_magic_packet(sock):
+    """
+    Test whether the magic packets are broadcasted to the specified network.
+
+    """
+    send_magic_packet(
+        '133713371337', '00-00-00-00-00-00', ip_address='example.com', port=7)
+    assert sock.mock_calls == [
+        call(socket.AF_INET, socket.SOCK_DGRAM),
+        call().setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1),
+        call().connect(('example.com', 7)),
+        call().send(
+            b'\xff\xff\xff\xff\xff\xff'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'),
+        call().send(
+            b'\xff\xff\xff\xff\xff\xff'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'),
+        call().close()
+    ]
+
+
+@patch('socket.socket', autospec=True)
+def test_send_magic_packet_default(sock):
+    """
+    Test whether the magic packets are broadcasted using default values.
+
+    """
+    send_magic_packet('133713371337', '00-00-00-00-00-00')
+    assert sock.mock_calls == [
+        call(socket.AF_INET, socket.SOCK_DGRAM),
+        call().setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1),
+        call().connect(('255.255.255.255', 9)),
+        call().send(
+            b'\xff\xff\xff\xff\xff\xff'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'
+            b'\x137\x137\x137'),
+        call().send(
+            b'\xff\xff\xff\xff\xff\xff'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'
+            b'\x00\x00\x00\x00\x00\x00'),
+        call().close()
+    ]
