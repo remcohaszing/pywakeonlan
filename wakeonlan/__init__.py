b659bfc8b36c9a3b1ea44908685bf31ebf1f4c43
David Pratt <davidpratt512@gmail.com>
2022-11-29 11:46:17 -0700

    Reorder imports for flake8

--- wakeonlan/__init__.py
+++ wakeonlan/__init__.py
@@ -1,5 +1,5 @@
 from .wakeonlan import BROADCAST_IP
 from .wakeonlan import DEFAULT_PORT
 from .wakeonlan import create_magic_packet
-from .wakeonlan import send_magic_packet
 from .wakeonlan import main
+from .wakeonlan import send_magic_packet
