3fdf8c9493df21e3e25fe3c19d14cdd8d010335e
David Pratt <davidpratt512@gmail.com>
2022-11-29 11:51:46 -0700

    Suppress flake8 F401

--- wakeonlan/__init__.py
+++ wakeonlan/__init__.py
@@ -3,3 +3,12 @@ from .wakeonlan import DEFAULT_PORT
 from .wakeonlan import create_magic_packet
 from .wakeonlan import main
 from .wakeonlan import send_magic_packet
+
+
+__all__ = (
+    "BROADCAST_IP",
+    "DEFAULT_PORT",
+    "create_magic_packet",
+    "send_magic_packet",
+    "main",
+)
