da59af73e2681afdbc7068ca58160f4c406d6574
Remco Haszing <remcohaszing@gmail.com>
2024-02-17 13:33:59 +0100

    Replace black and flake8 with ruff (#35)
    It’s simpler to use and has fewer dependencies.

--- docs/conf.py
+++ docs/conf.py
@@ -5,31 +5,31 @@ Configuration for the documentation generation.
 import pkg_resources
 
 
-project = "wakeonlan"
+project = 'wakeonlan'
 _dist = pkg_resources.get_distribution(project)
 
 version = _dist.version
 release = _dist.version
-copyright = "2012, Remco Haszing"
+copyright = '2012, Remco Haszing'
 
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon",
-    "sphinx_rtd_theme",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
 ]
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3.11", None),
+    'python': ('https://docs.python.org/3.11', None),
 }
 
-nitpick_ignore = [("py:class", "socket.AddressFamily")]
+nitpick_ignore = [('py:class', 'socket.AddressFamily')]
 
 nitpicky = True
 
-default_role = "any"
+default_role = 'any'
 todo_include_todos = True
 
-master_doc = "index"
-html_theme = "sphinx_rtd_theme"
+master_doc = 'index'
+html_theme = 'sphinx_rtd_theme'
